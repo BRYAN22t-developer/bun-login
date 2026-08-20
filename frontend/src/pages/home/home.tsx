5f8d62dfe40024b68f12becf3e7118a4cfc13ec1
Bryan <bybryan001@gmail.com>
2026-08-20 07:34:24 -0600

    feat: add home page

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
-  return <>
-    <header className="flex">
-      <p>Login Page</p>
-      <Link to="/user">Login</Link>
-    </header>
-
-  </>
+  return (
+    <>
+      <header className="bg-primary h-20">
+        <div className="flex px-2 justify-between items-center h-full text-background">
+          <p className="font-bold">Login Page</p>
+          <Link to="/user" className="bg-primary border-surface border-1 p-2 rounded-lg hover:bg-primary-hover transition-transform hover:scale-105">Login</Link>
+        </div>
+      </header>
+    </>
+  );
 }
