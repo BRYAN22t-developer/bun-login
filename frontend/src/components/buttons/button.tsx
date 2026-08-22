f04fc168128e36d3d661034c1f2a294ec2e5b237
Bryan <bybryan001@gmail.com>
2026-08-22 11:57:47 -0600

    style: improve bottons text appearance

--- frontend/src/components/buttons/button.tsx
+++ frontend/src/components/buttons/button.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from "react";
 
 const variants = {
   primary:
-    "bg-primary border-surface border-2 p-2 rounded-lg cursor-pointer hover:bg-primary-hover transition-transform hover:scale-105",
+    "bg-primary border-surface border-2 p-2 rounded-lg cursor-pointer hover:bg-primary-hover transition-transform hover:scale-105 text-surface",
   secondary:
     "items-center border-primary border-2 rounded-lg p-3 hover:border-primary-hover transition-transform hover:scale-105",
 };
--- frontend/src/components/buttons/link-button.tsx
+++ frontend/src/components/buttons/link-button.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const variants = {
   primary:
-    "bg-primary border-surface border-2 p-2 rounded-lg cursor-pointer hover:bg-primary-hover transition-transform hover:scale-105",
+    "bg-primary border-surface border-2 p-2 rounded-lg cursor-pointer hover:bg-primary-hover transition-transform hover:scale-105 text-surface",
   secondary:
     "items-center border-primary border-2 rounded-lg p-3 hover:border-primary-hover transition-transform hover:scale-105",
 };
