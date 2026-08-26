e30af865e241a28a850728db4f62387d230aa98c
Bryan <bybryan001@gmail.com>
2026-08-26 16:19:46 -0600

    feat: add options to sign jwt

--- backend/services/jose-jwt.service.ts
+++ backend/services/jose-jwt.service.ts
@@ -1,12 +1,16 @@
 import { SignJWT, jwtVerify } from "jose";
 
+type SignOptions = {
+  expirationTime: string,
+}
+
 export class JoseJwtService {
   private readonly secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  async sign(payload: Record<string, unknown>) {
+  async sign(payload: Record<string, unknown>, options?: SignOptions) {
     const jwt = await new SignJWT( payload )
       .setProtectedHeader({ alg: "HS256" })
       .setIssuedAt()
-      .setExpirationTime("2h")
+      .setExpirationTime(options?.expirationTime ?? "2h")
       .sign(this.secret);
 
     return jwt;
