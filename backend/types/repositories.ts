b9862d39f2a75c70c946dbebbb7839e90975ff11
Bryan <bybryan001@gmail.com>
2026-09-01 06:07:08 -0600

    feat: define refresh tokens repository

--- backend/types/repositories.ts
+++ backend/types/repositories.ts
@@ -7,3 +7,17 @@ export interface UsersRepository {
   login(username: string, password: string): Promise<User | null>;
   findById(id: string): Promise<User | null>;
 }
+
+export type RefreshToken = {
+  id: string,
+  userId: string,
+  token: string,
+  expiresAt: Date,
+  revokedAt: Date | null,
+  createdAt: Date
+}
+
+export interface RefreshTokensRepository {
+  add(refreshToken: RefreshToken): Promise<RefreshToken | null>
+  getByToken(token: string): Promise<RefreshToken | null>
+}
