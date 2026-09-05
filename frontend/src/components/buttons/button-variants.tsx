ab9948ae206e33a637234c69f62df79e5eed8b7d
Bryan <bybryan001@gmail.com>
2026-09-05 00:28:10 -0600

    feat: add more button variants

--- frontend/src/components/buttons/button-variants.tsx
+++ frontend/src/components/buttons/button-variants.tsx
@@ -5,6 +5,12 @@ const variants = {
     "bg-primary border-surface border-2 p-2 rounded-lg cursor-pointer hover:bg-primary-hover transition-transform hover:scale-105 text-surface",
   secondary:
     "items-center border-primary border-2 rounded-lg p-3 cursor-pointer hover:border-primary-hover transition-transform hover:scale-105",
+  success:
+    "bg-success border-surface border-2 p-2 rounded-lg cursor-pointer hover:bg-success-hover transition-transform hover:scale-105 text-surface",
+  warning:
+    "bg-warning border-surface border-2 p-2 rounded-lg cursor-pointer hover:bg-warning-hover transition-transform hover:scale-105 text-surface",
+  danger:
+    "bg-danger border-surface border-2 p-2 rounded-lg cursor-pointer hover:bg-danger-hover transition-transform hover:scale-105 text-surface",
 };
 
 export type Variant = keyof typeof variants
