ffec7950e141cd617bf0357b729ad801b39a9c6e
Bryan <bybryan001@gmail.com>
2026-09-17 04:33:59 -0600

    feat: add delete user action to frontend

--- frontend/src/pages/config/config.tsx
+++ frontend/src/pages/config/config.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/buttons/button";
 import { LinkButton } from "@/components/buttons/link-button";
 import { config } from "@/config";
 import { useAuth } from "@/context/auth";
 import { apiRequest } from "@/utils/api";
+import { IconTrash } from "@tabler/icons-react";
 import { useState, useEffect } from "react";
 
 type User = {
@@ -18,8 +20,9 @@ export function ConfigPage() {
     <>
       <p className="mb-4">Access Token: {accessToken}</p>
       <LinkButton to="/">Go Home</LinkButton>
-      <div className="m-10 rounded-lg overflow-hidden border border-transparent"><UsersTable /></div>
-
+      <div className="m-10 rounded-lg overflow-hidden border border-transparent">
+        <UsersTable />
+      </div>
     </>
   );
 }
@@ -31,7 +34,7 @@ function UsersTable() {
     getUsers()
       .then((res) => setUsers(res))
       .catch((err) => alert(`Error: ${err}`));
-  }, []);
+  }, [users]);
 
   return (
     <table className="w-full text-left border-collapse border-primary">
@@ -41,16 +44,45 @@ function UsersTable() {
           <th className="p-3 font-semibold">email</th>
           <th className="p-3 font-semibold">username</th>
           <th className="p-3 font-semibold">provider</th>
+          <th className="p-3 font-semibold">actions</th>
         </tr>
       </thead>
 
       <tbody>
         {users?.map((user) => (
-          <tr key={user.id} className="border-b border-gray-100 hover:bg-gray-50">
+          <tr
+            key={user.id}
+            className="border-b border-gray-100 hover:bg-gray-50"
+          >
             <td className="p-3">{user.id}</td>
             <td className="p-3">{user.email}</td>
             <td className="p-3">{user.username}</td>
             <td className="p-3">{user.provider ?? "Credentials"}</td>
+            <td className="p-3">
+              <Button
+                className="bg-danger hover:bg-danger-hover"
+                onClick={() => {
+                  const deleteUser = async () => {
+                    const res = await apiRequest(
+                      `${config.BACKEND_BASE_URL}/users/${user.id}`,
+                      {
+                        method: "DELETE",
+                      },
+                    );
+
+                    if (!res.ok) {
+                      throw new Error("Error deleting user");
+                    }
+                  };
+
+                  deleteUser()
+                    .then((res) => setUsers(null))
+                    .catch((err) => alert(`Error: ${err}`));
+                }}
+              >
+                <IconTrash />
+              </Button>
+            </td>
           </tr>
         ))}
       </tbody>
